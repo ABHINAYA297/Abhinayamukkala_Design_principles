6d721edda06dd2e7dc1085856e91aa540ac09e8f
ABHINAYA297 <abhinayarani225@gmail.com>
2020-02-10 23:37:02 +0530

    Design Principles

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main(String args[]) {
+		Calculator obj=new Calculator();
+		System.out.println(obj.Add(12,2));
+	    System.out.println(obj.Sub(29,7));
+	    System.out.println(obj.Mul(7,29));
+	    System.out.println(obj.Div(96,2));
+	    System.out.println(obj.Mod(30, 15));
+	}
+
+}
